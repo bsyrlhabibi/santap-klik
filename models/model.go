ac7f79c96cc1d5b138f3a004e8400101ed405052
bsyrlhabibi <84255918+bsyrlhabibi@users.noreply.github.com>
2023-11-02 09:09:18 +0700

    Update model.go

--- models/model.go
+++ models/model.go
@@ -4,35 +4,35 @@ import (
 	"fmt"
 	"santapKlik/configs"
 
-	"github.com/sirupsen/logrus"
+	"log"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-// func InitModel(c configs.ProgramConfig) *gorm.DB {
-// 	connStr := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-// 		c.DBUser,
-// 		c.DBPass,
-// 		c.DBHost,
-// 		c.DBPort,
-// 		c.DBName)
-// 	db, err := gorm.Open(mysql.Open(connStr), &gorm.Config{})
-// 	if err != nil {
-// 		log.Fatal("cannot connect database, ", err.Error())
-// 	}
-
-// 	return db
-// }
-func InitModel(config configs.ProgramConfig) *gorm.DB {
-	var dsn = fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", config.DBUser, config.DBPass, config.DBHost, config.DBPort, config.DBName)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+func InitModel(c configs.ProgramConfig) *gorm.DB {
+	connStr := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		c.DBUser,
+		c.DBPass,
+		c.DBHost,
+		c.DBPort,
+		c.DBName)
+	db, err := gorm.Open(mysql.Open(connStr), &gorm.Config{})
 	if err != nil {
-		logrus.Error("Model : cannot connect to database, ", err.Error())
-		return nil
+		log.Fatal("cannot connect database, ", err.Error())
 	}
 
 	return db
 }
+// func InitModel(config configs.ProgramConfig) *gorm.DB {
+// 	var dsn = fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", config.DBUser, config.DBPass, config.DBHost, config.DBPort, config.DBName)
+// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+// 	if err != nil {
+// 		logrus.Error("Model : cannot connect to database, ", err.Error())
+// 		return nil
+// 	}
+
+// 	return db
+// }
 
 func Migrate(db *gorm.DB) {
 	db.AutoMigrate(&Admin{})
