b9c3a527113d8f6b8eb34ea2d40a4044f1908b8e
bsyrlhabibi <84255918+bsyrlhabibi@users.noreply.github.com>
2023-11-02 08:59:39 +0700

    Update model.go

--- models/model.go
+++ models/model.go
@@ -9,16 +9,26 @@ import (
 	"gorm.io/gorm"
 )
 
-func InitModel(c configs.ProgramConfig) *gorm.DB {
-	connStr := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		c.DBUser,
-		c.DBPass,
-		c.DBHost,
-		c.DBPort,
-		c.DBName)
-	db, err := gorm.Open(mysql.Open(connStr), &gorm.Config{})
+// func InitModel(c configs.ProgramConfig) *gorm.DB {
+// 	connStr := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+// 		c.DBUser,
+// 		c.DBPass,
+// 		c.DBHost,
+// 		c.DBPort,
+// 		c.DBName)
+// 	db, err := gorm.Open(mysql.Open(connStr), &gorm.Config{})
+// 	if err != nil {
+// 		log.Fatal("cannot connect database, ", err.Error())
+// 	}
+
+// 	return db
+// }
+func InitModel(config configs.ProgramConfig) *gorm.DB {
+	var dsn = fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", config.DBUser, config.DBPass, config.DBHost, config.DBPort, config.DBName)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal("cannot connect database, ", err.Error())
+		logrus.Error("Model : cannot connect to database, ", err.Error())
+		return nil
 	}
 
 	return db
